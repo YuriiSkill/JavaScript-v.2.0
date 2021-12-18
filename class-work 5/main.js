be9c9f1ef5fec06a493b21f81c5ec815e3eeb36f
YuriiSkill <Rhodenskilled@gmail.com>
2021-12-18 17:37:03 +0200

    тренируемся писать циклы

--- class-work 5/main.js
+++ class-work 5/main.js
@@ -6,18 +6,45 @@
 //   if(i==4) break;
 // }
 
-let div = document.querySelectorAll('.one');
-console.log(div);
-// div.style.background='red';
+// let div = document.querySelectorAll('.one');
+// console.log(div);
+// // div.style.background='red';
 
-for(let i=0; i<div.length;i= i+1){
-  // console.log(i);
-  console.log(div[i]);
+// for(let i=0; i<div.length;i= i+1){
+//   // console.log(i);
+//   console.log(div[i]);
 
-  div[i].style.background = 'red';
-   div[i].onclick = two;
+//   div[i].style.background = 'red';
+//    div[i].onclick = two;
+// }
+
+// function two(){
+//   console.log('work!!!!');
+// } 
+
+// let b =document.getElementsByClassName('one');
+// let c =document.getElementsByTagName('div');
+// console.log(b);
+// console.log(c);
+
+// for (let i=0;i<b.length;i++){
+//   b[i].style.border = '3px solid black';
+// }
+
+
+
+
+
+
+
+document.querySelector('button').onclick=()=>{
+  let r = document.querySelectorAll('input[type="radio"]');
+  console.log(r);
+  for (let i=0;i<r.length;i++){
+    if(r[i].checked){
+      console.log(r[i].value);
+    }
+}
 }
 
-function two(){
-  console.log('work!!!!');
-}
+
